714e05c29da228fa5e4f9265e7e1294b740ffd95
yen <yen.work.0318@gmail.com>
2022-07-26 14:30:25 +0800

    change display style

--- lib/main.dart
+++ lib/main.dart
@@ -30,19 +30,21 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   static const platform = MethodChannel('yen.flutter.dev/kotlin');
-  final bool isPipMode = true;
+  final TextEditingController _notficationTitle = TextEditingController();
+  final TextEditingController _notficationDetail = TextEditingController();
 
-  String _batteryLevel = '電池狀態未知';
-  String _notificationInfo = '未發送';
-  String _isNativeView = '未顯示';
+  String _batteryLevel = '';
+  String _notificationInfo = '';
+  String _isNativeView = '';
+  String _exceptionInfo = '';
 
   Future<void> _getBatteryLevel() async {
     String batteryLevel;
     try {
       final int result = await platform.invokeMethod('getBatteryLevel');
-      batteryLevel = 'Battery level at $result % .';
-    } on PlatformException catch (e) {
-      batteryLevel = "Failed to get battery level: '${e.message}'.";
+      batteryLevel = '$result %';
+    } catch (e) {
+      batteryLevel = "Failed to get battery level: '$e'.";
     }
 
     setState(() {
@@ -53,9 +55,11 @@ class _MyHomePageState extends State<MyHomePage> {
   Future<void> _getNotfication() async {
     String notificationInfo;
     try {
-      notificationInfo = await platform.invokeMethod('getNotfication',{'title':'測試', 'detail':'發送通知發送通知'});
-    } on PlatformException catch (e) {
-      notificationInfo = "Failed to get notfication: '${e.message}'.";
+      FocusScope.of(context).requestFocus(FocusNode()); //remove focus
+      notificationInfo = await platform.invokeMethod('getNotfication',
+          {'title': _notficationTitle.text, 'detail': _notficationDetail.text});
+    } catch (e) {
+      notificationInfo = "Failed to get notfication: '$e'.";
     }
 
     setState(() {
@@ -67,8 +71,8 @@ class _MyHomePageState extends State<MyHomePage> {
     String isNativeView;
     try {
       isNativeView = await platform.invokeMethod('showNativeView');
-    } on PlatformException catch (e) {
-      isNativeView = "Failed to showNativeView: '${e.message}'.";
+    } catch (e) {
+      isNativeView = "Failed to showNativeView: '$e'.";
     }
 
     setState(() {
@@ -76,29 +80,111 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  Future<void> _getException() async {
+    String exceptionInfo;
+    try {
+      exceptionInfo = await platform.invokeMethod('getException');
+    } on PlatformException catch (e) {
+      exceptionInfo = "Failed PlatformException: '${e.message}'.";
+    } catch (e) {
+      exceptionInfo = "Failed Exception: '$e'.";
+    }
+
+    setState(() {
+      _exceptionInfo = exceptionInfo;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      child: Scaffold(
+        appBar: AppBar(title: const Text('原生溝通測試')),
+        body: Center(
+          child: ListView(
+            padding: EdgeInsets.all(10),
             children: [
-              ElevatedButton(
-                onPressed: _getBatteryLevel,
-                child: const Text('電池狀態'),
+              Card(
+                child: Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                          Icon(
+                            Icons.battery_full,
+                            color: Colors.grey,
+                          ),
+                          Text(_batteryLevel),
+                        ]),
+                        
+                        ElevatedButton(
+                          onPressed: _getBatteryLevel,
+                          child: const Text('電池狀態'),
+                        ),
+                      ]),
+                ),
               ),
-              Text(_batteryLevel),
-              ElevatedButton(
-                onPressed: _getNotfication,
-                child: const Text('通知'),
+              Card(
+                child: Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        TextField(
+                          decoration: InputDecoration(
+                            label: Text("標題"),
+                            suffixIcon: IconButton(
+                              onPressed: _notficationTitle.clear,
+                              icon: Icon(Icons.clear),
+                            ),
+                          ),
+                          controller: _notficationTitle,
+                        ),
+                        SizedBox(
+                          width: 30,
+                        ),
+                        TextField(
+                          decoration: InputDecoration(
+                            label: Text("內容"),
+                            suffixIcon: IconButton(
+                              onPressed: _notficationDetail.clear,
+                              icon: Icon(
+                                Icons.clear,
+                              ),
+                            ),
+                          ),
+                          controller: _notficationDetail,
+                        ),
+                        Text(_notificationInfo),
+                        ElevatedButton(
+                          onPressed: _getNotfication,
+                          child: const Text('發送通知'),
+                        ),
+                      ]),
+                ),
               ),
-              Text(_notificationInfo),
+              Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Flexible(child: Text(_exceptionInfo)),
+                        ElevatedButton(
+                          onPressed: _getException,
+                          child: const Text('原生端找不到func'),
+                        ),
+                      ]),
+                ),
+              ),
               ElevatedButton(
                 onPressed: _showNativeView,
-                child: const Text('顯示小視窗'),
+                child: const Text('顯示子母畫面'),
               ),
-              Text(_isNativeView)
+              Text(_isNativeView),
             ],
           ),
         ),
